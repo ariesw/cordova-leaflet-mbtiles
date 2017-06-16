d7080e90b99eb1c195e7380a6778eacc6601109e
Kieran David Evans <keyz182@gmail.com>
2017-06-16 17:47:49 +0100

    first commit

---
+++ www/CordovaLeafletMBTiles.js
@@ -0,0 +1,153 @@
+var loadingCss = "<style type='text/css'>@-webkit-keyframes uil-default-anim { 0% { opacity: 1} 100% {opacity: 0} }@keyframes uil-default-anim { 0% { opacity: 1} 100% {opacity: 0} }.uil-default-css > div:nth-of-type(1){-webkit-animation: uil-default-anim 1s linear infinite;animation: uil-default-anim 1s linear infinite;-webkit-animation-delay: -1s;animation-delay: -1s;}.uil-default-css { position: relative;background:none;width:200px;height:200px;}.uil-default-css > div:nth-of-type(2){-webkit-animation: uil-default-anim 1s linear infinite;animation: uil-default-anim 1s linear infinite;-webkit-animation-delay: -0.9166666666666666s;animation-delay: -0.9166666666666666s;}.uil-default-css { position: relative;background:none;width:200px;height:200px;}.uil-default-css > div:nth-of-type(3){-webkit-animation: uil-default-anim 1s linear infinite;animation: uil-default-anim 1s linear infinite;-webkit-animation-delay: -0.8333333333333334s;animation-delay: -0.8333333333333334s;}.uil-default-css { position: relative;background:none;width:200px;height:200px;}.uil-default-css > div:nth-of-type(4){-webkit-animation: uil-default-anim 1s linear infinite;animation: uil-default-anim 1s linear infinite;-webkit-animation-delay: -0.75s;animation-delay: -0.75s;}.uil-default-css { position: relative;background:none;width:200px;height:200px;}.uil-default-css > div:nth-of-type(5){-webkit-animation: uil-default-anim 1s linear infinite;animation: uil-default-anim 1s linear infinite;-webkit-animation-delay: -0.6666666666666666s;animation-delay: -0.6666666666666666s;}.uil-default-css { position: relative;background:none;width:200px;height:200px;}.uil-default-css > div:nth-of-type(6){-webkit-animation: uil-default-anim 1s linear infinite;animation: uil-default-anim 1s linear infinite;-webkit-animation-delay: -0.5833333333333334s;animation-delay: -0.5833333333333334s;}.uil-default-css { position: relative;background:none;width:200px;height:200px;}.uil-default-css > div:nth-of-type(7){-webkit-animation: uil-default-anim 1s linear infinite;animation: uil-default-anim 1s linear infinite;-webkit-animation-delay: -0.5s;animation-delay: -0.5s;}.uil-default-css { position: relative;background:none;width:200px;height:200px;}.uil-default-css > div:nth-of-type(8){-webkit-animation: uil-default-anim 1s linear infinite;animation: uil-default-anim 1s linear infinite;-webkit-animation-delay: -0.4166666666666667s;animation-delay: -0.4166666666666667s;}.uil-default-css { position: relative;background:none;width:200px;height:200px;}.uil-default-css > div:nth-of-type(9){-webkit-animation: uil-default-anim 1s linear infinite;animation: uil-default-anim 1s linear infinite;-webkit-animation-delay: -0.3333333333333333s;animation-delay: -0.3333333333333333s;}.uil-default-css { position: relative;background:none;width:200px;height:200px;}.uil-default-css > div:nth-of-type(10){-webkit-animation: uil-default-anim 1s linear infinite;animation: uil-default-anim 1s linear infinite;-webkit-animation-delay: -0.25s;animation-delay: -0.25s;}.uil-default-css { position: relative;background:none;width:200px;height:200px;}.uil-default-css > div:nth-of-type(11){-webkit-animation: uil-default-anim 1s linear infinite;animation: uil-default-anim 1s linear infinite;-webkit-animation-delay: -0.16666666666666666s;animation-delay: -0.16666666666666666s;}.uil-default-css { position: relative;background:none;width:200px;height:200px;}.uil-default-css > div:nth-of-type(12){-webkit-animation: uil-default-anim 1s linear infinite;animation: uil-default-anim 1s linear infinite;-webkit-animation-delay: -0.08333333333333333s;animation-delay: -0.08333333333333333s;}.uil-default-css { position: relative;background:none;width:200px;height:200px;}</style><div class='uil-default-css' style='transform:scale(0.6);'><div style='top:80px;left:93px;width:14px;height:40px;background:#00b2ff;-webkit-transform:rotate(0deg) translate(0,-60px);transform:rotate(0deg) translate(0,-60px);border-radius:10px;position:absolute;'></div><div style='top:80px;left:93px;width:14px;height:40px;background:#00b2ff;-webkit-transform:rotate(30deg) translate(0,-60px);transform:rotate(30deg) translate(0,-60px);border-radius:10px;position:absolute;'></div><div style='top:80px;left:93px;width:14px;height:40px;background:#00b2ff;-webkit-transform:rotate(60deg) translate(0,-60px);transform:rotate(60deg) translate(0,-60px);border-radius:10px;position:absolute;'></div><div style='top:80px;left:93px;width:14px;height:40px;background:#00b2ff;-webkit-transform:rotate(90deg) translate(0,-60px);transform:rotate(90deg) translate(0,-60px);border-radius:10px;position:absolute;'></div><div style='top:80px;left:93px;width:14px;height:40px;background:#00b2ff;-webkit-transform:rotate(120deg) translate(0,-60px);transform:rotate(120deg) translate(0,-60px);border-radius:10px;position:absolute;'></div><div style='top:80px;left:93px;width:14px;height:40px;background:#00b2ff;-webkit-transform:rotate(150deg) translate(0,-60px);transform:rotate(150deg) translate(0,-60px);border-radius:10px;position:absolute;'></div><div style='top:80px;left:93px;width:14px;height:40px;background:#00b2ff;-webkit-transform:rotate(180deg) translate(0,-60px);transform:rotate(180deg) translate(0,-60px);border-radius:10px;position:absolute;'></div><div style='top:80px;left:93px;width:14px;height:40px;background:#00b2ff;-webkit-transform:rotate(210deg) translate(0,-60px);transform:rotate(210deg) translate(0,-60px);border-radius:10px;position:absolute;'></div><div style='top:80px;left:93px;width:14px;height:40px;background:#00b2ff;-webkit-transform:rotate(240deg) translate(0,-60px);transform:rotate(240deg) translate(0,-60px);border-radius:10px;position:absolute;'></div><div style='top:80px;left:93px;width:14px;height:40px;background:#00b2ff;-webkit-transform:rotate(270deg) translate(0,-60px);transform:rotate(270deg) translate(0,-60px);border-radius:10px;position:absolute;'></div><div style='top:80px;left:93px;width:14px;height:40px;background:#00b2ff;-webkit-transform:rotate(300deg) translate(0,-60px);transform:rotate(300deg) translate(0,-60px);border-radius:10px;position:absolute;'></div><div style='top:80px;left:93px;width:14px;height:40px;background:#00b2ff;-webkit-transform:rotate(330deg) translate(0,-60px);transform:rotate(330deg) translate(0,-60px);border-radius:10px;position:absolute;'></div></div> Trying to load the tile";
+
+var fromCharCode = function(bytes){
+    // var bytes = new ByteArray(buffer);
+    var array = [];
+    var offset = 0;
+    var charCode = 0;
+    var end = bytes.length;
+    var strings = [];
+    var chunkSize = 0xffff;
+    while(offset < end){
+        charCode = bytes[offset];
+        offset += 1;
+
+        array.push(charCode);
+
+        if( array.length == chunkSize ) {
+            strings.push(String.fromCharCode.apply(null, array));
+            array = [];
+        }
+    }
+    strings.push(String.fromCharCode.apply(null, array));
+    return strings.join('');
+}
+
+var hexToB64 = function (inStr) {
+    var hexArray = inStr
+        .replace(/\r|\n/g, "")
+        .replace(/([\da-fA-F]{2}) ?/g, "0x$1 ")
+        .replace(/ +$/, "")
+        .split(" ");
+
+    var strings = [];
+    var chunkSize = 0xffff;
+
+    var byteString = fromCharCode(hexArray);
+    return window.btoa(byteString);
+};
+
+
+if(!window.hasOwnProperty('L')){
+    console.error("Leaflet not found!")
+}else{
+    L.GridLayer.MBTilesLayer = L.TileLayer.extend({
+        db: null,
+        initialize: function(url, options) {
+            var that = this;
+
+            if(options.debug){
+                that.debug = options.debug;
+            }
+
+            document.addEventListener('deviceready', function() {
+                window.sqlitePlugin.openDatabase({name: options.filename, location: 'default'},
+                    function success(db){
+                        that.db = db;
+                        console.log("Database opened - redrawing...")
+                        that.redraw();
+                    }, function error(err){
+                        console.log('Open database ERROR: ' + JSON.stringify(err));
+                    }
+                );
+            });
+
+            return L.TileLayer.prototype.initialize.call(this, url, options);
+        },
+        getTileUrl: function (data) {
+            return L.Util.template(this._url, L.extend(data, this.options));
+        },
+        addDebugHtml: function(tile, data){
+            if(this.debug){
+                tile.style.outline = '1px dashed red';
+                var text = document.createElement('div');
+                text.innerHTML =  "X:" + data.x + " | Y:" + data.y + " | -Y:" + data['-y'] + " | Z:" + data.z;
+                text.style="z-index:10; top:0; left:0; position: absolute";
+                tile.appendChild(text);
+            }
+            return tile;
+        },
+        createTile: function (coords, done) {
+            var that = this;
+            var tile = document.createElement('div');
+
+            var data = {
+                r: L.Browser.retina ? '@2x' : '',
+                s: this._getSubdomain(coords),
+                x: coords.x,
+                y: coords.y,
+                z: this._getZoomForUrl()
+            };
+            if (this._map && !this._map.options.crs.infinite) {
+                var invertedY = this._globalTileRange.max.y - coords.y;
+                if (this.options.tms) {
+                    data['y'] = invertedY;
+                }
+                data['-y'] = invertedY;
+            }
+
+            tile.innerHTML = loadingCss;
+
+            if(!that.db){
+                that.addDebugHtml(tile, data);
+                setTimeout(function(){
+                    done(null, tile);
+                },10);
+            }else{
+                that.db.executeSql('SELECT hex(tile_data) as tile_data FROM tiles WHERE zoom_level = ? AND tile_column = ? AND tile_row = ?',
+                    [data.z, data.x, data['-y']],
+                    function success(result){
+                        if (result.rows.length > 0) {
+                            try{
+                                var hex = result.rows.item(0).tile_data;
+                                var dataurl = '';
+                                dataurl = 'data:image/png;base64,' + hexToB64(hex);
+
+                                tile.innerHTML = '';
+                                that.addDebugHtml(tile, data);
+
+                                tile.style.backgroundImage = "url('" + dataurl + "')";
+                            }catch(e){
+                                console.log(e);
+                            }
+
+                        }else{
+                            try{
+                                var link = that.getTileUrl(data);
+
+                                var img = document.createElement('img');
+                                img.src = link;
+                                img.style = 'top:0;left:0;height:100%;width:100%;position:absolute;';
+
+                                that.addDebugHtml(tile, data);
+
+                                tile.appendChild(img);
+                            }catch(e){
+                                console.err(e);
+                            }
+                        }
+
+                        done(null, tile);
+                    }, function(error){
+                        console.log(error);
+                    })
+            }
+
+            return tile;
+        }
+    });
+
+    L.gridLayer.mbTilesLayer = function(url,opts) {
+        return new L.GridLayer.MBTilesLayer(url,opts);
+    };
+}
