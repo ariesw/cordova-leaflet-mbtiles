9b94512abd450bf59bc143e412302b18d2d7e069
Kieran Evans <keyz182@users.noreply.github.com>
2017-06-23 15:43:04 +0100

    Android implementation of hex to base 64

---
+++ src/src/android/MBTiles.java
@@ -0,0 +1,33 @@
+package uk.co.dbyz.cordova.leaflet-mbtiles;
+
+import org.apache.cordova.*;
+import org.apache.commons.codec.binary.*;
+import org.json.JSONArray;
+import org.json.JSONException;
+
+public class MBTiles extends CordovaPlugin {
+
+    @Override
+    public boolean execute(String action, JSONArray data, CallbackContext callbackContext) throws JSONException {
+
+        if (action.equals("hextob64")) {
+            //convert String to char array (1st step)
+            char[] charArray = data.getString(0).toCharArray();
+            
+            // decode the char array to byte[] (2nd step)
+            byte[] decodedHex = Hex.decodeHex(charArray);
+
+            // The String decoded to Base64 (3rd step)
+            String result= Base64.encodeBase64String(decodedHex);
+            
+            callbackContext.success(result);
+
+            return true;
+
+        } else {
+            
+            return false;
+
+        }
+    }
+}
