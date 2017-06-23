37e156705f7050adad43e0e6349121083e9bbe70
Kieran Evans <keyz182@users.noreply.github.com>
2017-06-23 15:33:56 +0100

    Some cleanup

--- www/mbtiles.js
+++ www/mbtiles.js
@@ -1,6 +1,6 @@
 var loadingCss = "<style type='text/css'>@-webkit-keyframes uil-default-anim { 0% { opacity: 1} 100% {opacity: 0} }@keyframes uil-default-anim { 0% { opacity: 1} 100% {opacity: 0} }.uil-default-css > div:nth-of-type(1){-webkit-animation: uil-default-anim 1s linear infinite;animation: uil-default-anim 1s linear infinite;-webkit-animation-delay: -1s;animation-delay: -1s;}.uil-default-css { position: relative;background:none;width:200px;height:200px;}.uil-default-css > div:nth-of-type(2){-webkit-animation: uil-default-anim 1s linear infinite;animation: uil-default-anim 1s linear infinite;-webkit-animation-delay: -0.9166666666666666s;animation-delay: -0.9166666666666666s;}.uil-default-css { position: relative;background:none;width:200px;height:200px;}.uil-default-css > div:nth-of-type(3){-webkit-animation: uil-default-anim 1s linear infinite;animation: uil-default-anim 1s linear infinite;-webkit-animation-delay: -0.8333333333333334s;animation-delay: -0.8333333333333334s;}.uil-default-css { position: relative;background:none;width:200px;height:200px;}.uil-default-css > div:nth-of-type(4){-webkit-animation: uil-default-anim 1s linear infinite;animation: uil-default-anim 1s linear infinite;-webkit-animation-delay: -0.75s;animation-delay: -0.75s;}.uil-default-css { position: relative;background:none;width:200px;height:200px;}.uil-default-css > div:nth-of-type(5){-webkit-animation: uil-default-anim 1s linear infinite;animation: uil-default-anim 1s linear infinite;-webkit-animation-delay: -0.6666666666666666s;animation-delay: -0.6666666666666666s;}.uil-default-css { position: relative;background:none;width:200px;height:200px;}.uil-default-css > div:nth-of-type(6){-webkit-animation: uil-default-anim 1s linear infinite;animation: uil-default-anim 1s linear infinite;-webkit-animation-delay: -0.5833333333333334s;animation-delay: -0.5833333333333334s;}.uil-default-css { position: relative;background:none;width:200px;height:200px;}.uil-default-css > div:nth-of-type(7){-webkit-animation: uil-default-anim 1s linear infinite;animation: uil-default-anim 1s linear infinite;-webkit-animation-delay: -0.5s;animation-delay: -0.5s;}.uil-default-css { position: relative;background:none;width:200px;height:200px;}.uil-default-css > div:nth-of-type(8){-webkit-animation: uil-default-anim 1s linear infinite;animation: uil-default-anim 1s linear infinite;-webkit-animation-delay: -0.4166666666666667s;animation-delay: -0.4166666666666667s;}.uil-default-css { position: relative;background:none;width:200px;height:200px;}.uil-default-css > div:nth-of-type(9){-webkit-animation: uil-default-anim 1s linear infinite;animation: uil-default-anim 1s linear infinite;-webkit-animation-delay: -0.3333333333333333s;animation-delay: -0.3333333333333333s;}.uil-default-css { position: relative;background:none;width:200px;height:200px;}.uil-default-css > div:nth-of-type(10){-webkit-animation: uil-default-anim 1s linear infinite;animation: uil-default-anim 1s linear infinite;-webkit-animation-delay: -0.25s;animation-delay: -0.25s;}.uil-default-css { position: relative;background:none;width:200px;height:200px;}.uil-default-css > div:nth-of-type(11){-webkit-animation: uil-default-anim 1s linear infinite;animation: uil-default-anim 1s linear infinite;-webkit-animation-delay: -0.16666666666666666s;animation-delay: -0.16666666666666666s;}.uil-default-css { position: relative;background:none;width:200px;height:200px;}.uil-default-css > div:nth-of-type(12){-webkit-animation: uil-default-anim 1s linear infinite;animation: uil-default-anim 1s linear infinite;-webkit-animation-delay: -0.08333333333333333s;animation-delay: -0.08333333333333333s;}.uil-default-css { position: relative;background:none;width:200px;height:200px;}</style><div class='uil-default-css' style='transform:scale(0.6);'><div style='top:80px;left:93px;width:14px;height:40px;background:#00b2ff;-webkit-transform:rotate(0deg) translate(0,-60px);transform:rotate(0deg) translate(0,-60px);border-radius:10px;position:absolute;'></div><div style='top:80px;left:93px;width:14px;height:40px;background:#00b2ff;-webkit-transform:rotate(30deg) translate(0,-60px);transform:rotate(30deg) translate(0,-60px);border-radius:10px;position:absolute;'></div><div style='top:80px;left:93px;width:14px;height:40px;background:#00b2ff;-webkit-transform:rotate(60deg) translate(0,-60px);transform:rotate(60deg) translate(0,-60px);border-radius:10px;position:absolute;'></div><div style='top:80px;left:93px;width:14px;height:40px;background:#00b2ff;-webkit-transform:rotate(90deg) translate(0,-60px);transform:rotate(90deg) translate(0,-60px);border-radius:10px;position:absolute;'></div><div style='top:80px;left:93px;width:14px;height:40px;background:#00b2ff;-webkit-transform:rotate(120deg) translate(0,-60px);transform:rotate(120deg) translate(0,-60px);border-radius:10px;position:absolute;'></div><div style='top:80px;left:93px;width:14px;height:40px;background:#00b2ff;-webkit-transform:rotate(150deg) translate(0,-60px);transform:rotate(150deg) translate(0,-60px);border-radius:10px;position:absolute;'></div><div style='top:80px;left:93px;width:14px;height:40px;background:#00b2ff;-webkit-transform:rotate(180deg) translate(0,-60px);transform:rotate(180deg) translate(0,-60px);border-radius:10px;position:absolute;'></div><div style='top:80px;left:93px;width:14px;height:40px;background:#00b2ff;-webkit-transform:rotate(210deg) translate(0,-60px);transform:rotate(210deg) translate(0,-60px);border-radius:10px;position:absolute;'></div><div style='top:80px;left:93px;width:14px;height:40px;background:#00b2ff;-webkit-transform:rotate(240deg) translate(0,-60px);transform:rotate(240deg) translate(0,-60px);border-radius:10px;position:absolute;'></div><div style='top:80px;left:93px;width:14px;height:40px;background:#00b2ff;-webkit-transform:rotate(270deg) translate(0,-60px);transform:rotate(270deg) translate(0,-60px);border-radius:10px;position:absolute;'></div><div style='top:80px;left:93px;width:14px;height:40px;background:#00b2ff;-webkit-transform:rotate(300deg) translate(0,-60px);transform:rotate(300deg) translate(0,-60px);border-radius:10px;position:absolute;'></div><div style='top:80px;left:93px;width:14px;height:40px;background:#00b2ff;-webkit-transform:rotate(330deg) translate(0,-60px);transform:rotate(330deg) translate(0,-60px);border-radius:10px;position:absolute;'></div></div> Trying to load the tile";
 
-var fromCharCode = function(bytes){
+var fromCharCode = function (bytes) {
     // var bytes = new ByteArray(buffer);
     var array = [];
     var offset = 0;
@@ -8,13 +8,13 @@ var fromCharCode = function(bytes){
     var end = bytes.length;
     var strings = [];
     var chunkSize = 0xffff;
-    while(offset < end){
+    while (offset < end) {
         charCode = bytes[offset];
         offset += 1;
 
         array.push(charCode);
 
-        if( array.length == chunkSize ) {
+        if (array.length == chunkSize) {
             strings.push(String.fromCharCode.apply(null, array));
             array = [];
         }
@@ -38,26 +38,26 @@ var hexToB64 = function (inStr) {
 };
 
 
-if(!window.hasOwnProperty('L')){
+if (!window.hasOwnProperty('L')) {
     console.error("Leaflet not found!")
-}else{
+} else {
     L.GridLayer.MBTilesLayer = L.TileLayer.extend({
         db: null,
-        initialize: function(url, options) {
+        initialize: function (url, options) {
             var that = this;
 
-            if(options.debug){
+            if (options.debug) {
                 that.debug = options.debug;
             }
 
-            document.addEventListener('deviceready', function() {
+            document.addEventListener('deviceready', function () {
                 window.sqlitePlugin.openDatabase({name: options.filename, location: 'default'},
-                    function success(db){
+                    function success(db) {
                         that.db = db;
-                        console.log("Database opened - redrawing...")
+                        console.log("Database opened - redrawing...");
                         that.redraw();
-                    }, function error(err){
-                        console.log('Open database ERROR: ' + JSON.stringify(err));
+                    }, function error(err) {
+                        console.error('Open database ERROR: ' + JSON.stringify(err));
                     }
                 );
             });
@@ -67,39 +67,39 @@ if(!window.hasOwnProperty('L')){
         getTileUrl: function (data) {
             return L.Util.template(this._url, L.extend(data, this.options));
         },
-        addDebugHtml: function(tile, data){
-            if(this.debug){
+        addDebugHtml: function (tile, data) {
+            if (this.debug) {
                 tile.style.outline = '1px dashed red';
                 var text = document.createElement('div');
-                text.innerHTML =  "X:" + data.x + " | Y:" + data.y + " | -Y:" + data['-y'] + " | Z:" + data.z;
-                text.style="z-index:10; top:0; left:0; position: absolute";
+                text.innerHTML = "X:" + data.x + " | Y:" + data.y + " | -Y:" + data['-y'] + " | Z:" + data.z;
+                text.style = "z-index:10; top:0; left:0; position: absolute";
                 tile.appendChild(text);
             }
             return tile;
         },
-        getTileFromDb: function(data){
+        getTileFromDb: function (data, db) {
             var that = this;
 
-            return new Promise(function promise(resolve, reject){
-                that.db.executeSql('SELECT hex(tile_data) as tile_data FROM tiles WHERE zoom_level = ? AND tile_column = ? AND tile_row = ?',
+            return new Promise(function promise(resolve, reject) {
+                db.executeSql('SELECT hex(tile_data) as tile_data FROM tiles WHERE zoom_level = ? AND tile_column = ? AND tile_row = ?',
                     [data.z, data.x, data['-y']],
-                    function success(result){
+                    function success(result) {
                         if (result.rows.length > 0) {
-                            try{
+                            try {
                                 var hex = result.rows.item(0).tile_data;
                                 var dataurl = '';
                                 dataurl = 'data:image/png;base64,' + hexToB64(hex);
 
-                                resolve({'dataurl' : dataurl});
-                            }catch(e){
-                                console.log(e);
+                                resolve({'dataurl': dataurl});
+                            } catch (e) {
+                                if(that.debug) console.log(e);
                                 reject(e);
                             }
-                        }else{
+                        } else {
                             reject(-1);
                         }
-                    }, function(error){
-                        console.log(error);
+                    }, function (error) {
+                        if(that.debug) console.log(error);
                         reject(error);
                     });
             });
@@ -126,33 +126,44 @@ if(!window.hasOwnProperty('L')){
 
             tile.innerHTML = loadingCss;
 
-            if(!that.db){
+            if (!that.db) {
+                var img = document.createElement('img');
+                img.src = that.getTileUrl(data);
+                img.style = 'top:0;left:0;height:100%;width:100%;position:absolute;';
+
                 that.addDebugHtml(tile, data);
-                setTimeout(function(){
+
+                setTimeout(function () {
                     done(null, tile);
-                },10);
-            }else{
+                }, 1);
+            } else {
                 //TODO iterate over list of MBTiles DBs
-                var promises = [ that.getTileFromDb(data)];
+                var promises = [that.getTileFromDb(data, that.db)];
 
                 //Converts promise rejects and successes into uniform successes so promise.all works
-                function reflect(promise){
-                    return promise.then(function(v){ return {v:v, status: "resolved" }},
-                        function(e){ return {e:e, status: "rejected" }});
+                function reflect(promise) {
+                    return promise.then(function (v) {
+                            return {v: v, status: "resolved"}
+                        },
+                        function (e) {
+                            return {e: e, status: "rejected"}
+                        });
                 }
 
-                Promise.all(promises.map(reflect)).then(function(results){
+                Promise.all(promises.map(reflect)).then(function (results) {
+                    var error = null; //Keep track of error if needed to pass to done
+
                     //Filter for successful results
-                    var success = results.filter(function(x){
-                       return x.status === "resolved";
+                    var success = results.filter(function (x) {
+                        return x.status === "resolved";
                     });
 
-                    var dataurls = success.filter(function(x){
+                    var dataurls = success.filter(function (x) {
                         return 'dataurl' in x.v;
                     });
 
-                    if(dataurls.length <= 0){
-                        try{
+                    if (dataurls.length <= 0) {
+                        try {
                             var img = document.createElement('img');
                             img.src = that.getTileUrl(data);
                             img.style = 'top:0;left:0;height:100%;width:100%;position:absolute;';
@@ -160,12 +171,12 @@ if(!window.hasOwnProperty('L')){
                             that.addDebugHtml(tile, data);
 
                             tile.appendChild(img);
-                        }catch(e){
+                        } catch (e) {
                             console.error("Couldn't get tile URL");
-                            console.error(e);
+                            if(that.debug) console.error(e);
+                            error = e;
                         }
-                    }else{
-                        //There was at least one successful result
+                    } else {
                         //TODO: Work out how to prioritise
                         tile.innerHTML = '';
                         that.addDebugHtml(tile, data);
@@ -173,7 +184,7 @@ if(!window.hasOwnProperty('L')){
                     }
 
                     //Whatever the result, call done on the tile
-                    done(null, tile);
+                    done(error, tile);
                 });
             }
 
@@ -181,7 +192,7 @@ if(!window.hasOwnProperty('L')){
         }
     });
 
-    L.gridLayer.mbTilesLayer = function(url,opts) {
-        return new L.GridLayer.MBTilesLayer(url,opts);
+    L.gridLayer.mbTilesLayer = function (url, opts) {
+        return new L.GridLayer.MBTilesLayer(url, opts);
     };
 }
