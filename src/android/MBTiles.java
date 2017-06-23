349f1fd86a701b01e2ce6319af8207e464531b29
Kieran David Evans <keyz182@gmail.com>
2017-06-23 16:20:21 +0100

    Call Fixes

--- src/android/MBTiles.java
+++ src/android/MBTiles.java
@@ -1,7 +1,8 @@
 package uk.co.dbyz.cordova.leaflet.mbtiles;
 
 import org.apache.cordova.*;
-import org.apache.commons.codec.binary.*;
+import org.apache.commons.codec.binary.Hex;
+import org.apache.commons.codec.binary.Base64;
 import org.apache.commons.codec.DecoderException;
 import org.json.JSONArray;
 import org.json.JSONException;
@@ -10,7 +11,7 @@ public class MBTiles extends CordovaPlugin {
 
     @Override
     public boolean execute(String action, JSONArray data, CallbackContext callbackContext) throws JSONException {
-
+		
         if (action.equals("hextob64")) {
             //convert String to char array (1st step)
             char[] charArray = data.getString(0).toCharArray();
@@ -20,7 +21,7 @@ public class MBTiles extends CordovaPlugin {
 				byte[] decodedHex = Hex.decodeHex(charArray);
 
 				// The String decoded to Base64 (3rd step)
-				String result= Base64.encodeBase64String(decodedHex);
+				String result= new String(Base64.encodeBase64(decodedHex));
 					
 				callbackContext.success(result);
 			}catch(DecoderException ex){
